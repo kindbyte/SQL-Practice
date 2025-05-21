53a5b77ac092367742c120d919455dc330e082c9
kindbyte <saglara.garyaeva0704@gmail.com>
2025-05-21 13:57:02 -0400

    Update salary_comparison.sql

--- queries/salary_comparison.sql
+++ queries/salary_comparison.sql
@@ -1,1 +1,12 @@
-
+SELECT e.id, e.name
+   FROM employees e
+      WHERE e.salary > (
+                         SELECT AVG(e2.salary)
+                            FROM employees e2
+                               WHERE e2.department_id = e.department_id
+                                                                        )
+                                  AND e.salary < (
+                                                  SELECT AVG(e3.salary)
+                                                     FROM employees e3
+                                                       WHERE e3.id = e.manager_id
+                                                                                  );
