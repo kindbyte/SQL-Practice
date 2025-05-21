6bc3d281c56ac55969f92c5c8bb7676e693ff933
kindbyte <saglara.garyaeva0704@gmail.com>
2025-05-21 13:58:09 -0400

    Update department_sales_2023.sql

--- queries/department_sales_2023.sql
+++ queries/department_sales_2023.sql
@@ -1,1 +1,11 @@
-
+SELECT departments.department_name, 
+              COALESCE(AVG(sales.sale_amount), 0) AS avg_sales,
+              COALESCE(MAX(sales.sale_amount), 0) AS max_sales,
+              COALESCE(MIN(sales.sale_amount), 0) AS min_sales 
+                  FROM employees 
+                       LEFT JOIN departments ON departments.department_id = employees.department_id 
+                       LEFT JOIN sales ON employees.employee_id = sales.employee_id 
+                               AND sales.sale_date BETWEEN ‘2023-01-01’ AND ‘2023-12-31’ 
+                                    GROUP BY departments.department_name 
+                                           HAVING AVG(sales.sale_amount) > 500 
+                                                  ORDER BY AVG(sales.sale_amount) DESC;
