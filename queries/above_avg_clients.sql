140d3b6815c62b25deb53eb4c7fe1e694d3f8334
kindbyte <saglara.garyaeva0704@gmail.com>
2025-05-21 13:59:53 -0400

    Update above_avg_clients.sql

--- queries/above_avg_clients.sql
+++ queries/above_avg_clients.sql
@@ -1,1 +1,10 @@
-
+SELECT c.client_id, c.name, c.city, c.income
+FROM clients c
+      JOIN (
+            SELECT city, AVG(income) AS avg_income
+            FROM clients
+            GROUP BY city
+            )clients_2 ON clients_2.city = c.city
+               WHERE c.income > clients_2.avg_income
+                  ORDER BY c.city, c.income DESC;  
+                  
